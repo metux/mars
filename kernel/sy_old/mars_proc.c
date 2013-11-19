5fa93bacb6f54c334e29aae14862c8215338f15d
Thomas Schoebel-Theuer <schoebel@bell.site>
2013-11-19 10:38:59 +0100

    net: allow compression of bulk data

--- kernel/sy_old/mars_proc.c
+++ kernel/sy_old/mars_proc.c
@@ -295,6 +295,7 @@ ctl_table mars_table[] = {
 #endif
 	// changing makes no sense because the server will immediately start upon modprobe
 	INT_ENTRY("mars_port",            mars_net_default_port,  0400),
+	INT_ENTRY("mars_compress_data",   mars_net_compress_data, 0600),
 	INT_ENTRY("network_io_timeout",   global_net_io_timeout,  0600),
 	{
 		_CTL_NAME
